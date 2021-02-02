e4507cdc69b9890544d4e3ef1cad1efb85020cdc
Konstanta <anastasia.corporate@gmail.com>
2021-02-02 17:53:59 +0300

    did much, SHOPPING CART

--- js/training_chill.js
+++ js/training_chill.js
@@ -1,19 +1,7 @@
-let numbers = "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"
+let clicks = 0;
 
-let splittedArr = numbers.split(' ');
-
-
-let numberArray = splittedArr.map((el)=>parseInt(el));
-console.log(numberArray)
-
-let sortedArray = numberArray.sort(function(a, b){return a-b});
-console.log("sorted" + " " + sortedArray)
-
-let min = sortedArray[0].toString();
-let max = sortedArray.slice(-1).pop().toString();
-
-let minMax = (max + " " + min);
-
-console.log(min);
-console.log(max);
-console.log(minMax);
+function clickME() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    document.getElementById('clicks').style.display = 'inline-flex';
+}
